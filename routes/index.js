3e70166db211d6a12bac6aa79e713c7919144396
victor-blanchard <159810800+victor-blanchard@users.noreply.github.com>
2024-11-21 11:04:14 +0100

    initialisation du backend

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+const fetch = require("node-fetch");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+module.exports = router;
